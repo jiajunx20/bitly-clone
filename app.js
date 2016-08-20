ea24f186948d5f5b18a14ddb43929d2bb1e58279
Jiajun <jxu@club-os.com>
2016-08-19 22:21:58 -0400

    begin phase 2

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var app = express();
 
-var router = require('./Routers/router');
+var rest = require('./Routers/rest');
 var redirect = require('./Routers/redirect');
-	
+
 app.longToShort = {};
 app.shortToLong = {};
 
+app.use('/public', express.static(__dirname + '/public'));
+
+app.use('/', indexRouter);
 app.use('/:shortUrl', redirect);
-app.use('/api/v1', router);
+app.use('/api/v1', rest);
 
 app.listen(3000);
-
