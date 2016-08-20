d94d176782dc34117deb2d081c24afbeafa3faec
Jiajun <jxu@club-os.com>
2016-08-20 12:18:15 -0400

    wrap up the controlllers

---
+++ public/js/controllers/urlController.js
@@ -0,0 +1,10 @@
+var app = angular.module("tinyUrlApp");
+
+app.controller("urlController", ["$scope", "$http", "routeParams", function($scope, $http, $routeParams) {
+    $http.get("/api/v1/urls/" + $routeParams.shortUrl)
+        .success(function(data) {
+            $scope.shortUrl = data.shortUrl;
+            $scope.longUrl = data.longUrl;
+
+        });
+}]);
