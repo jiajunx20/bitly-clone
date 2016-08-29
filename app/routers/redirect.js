3ae31c8b5cbe891f7615cad75b47f218d3c9ea8b
Jiajun <jxu@club-os.com>
2016-08-28 22:04:47 -0400

    some renaming

--- app/routers/redirect.js
+++ app/routers/redirect.js
@@ -4,9 +4,9 @@ var urlService = require('../services/urlService');
 
 router.get('*', function(req, res) {
     var shortUrl = req.originalUrl.slice(1);
-    var longUrl = urlService.getLongUrl(shortUrl, function(longUrl) {
-        if (url) {
-            res.redirect(longUrl);
+    var longUrl = urlService.getLongUrl(shortUrl, function(data) {
+        if (data) {
+            res.redirect(data.longUrl);
         } else {
             res.sendFile('../public/views/error.html');
         }
